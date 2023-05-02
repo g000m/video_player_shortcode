b47baa1671ef7e9d311af077359c1842464502d6
Gabe Herbert <gh@gabeherbert.com>
2023-05-02 17:53:02 +0200

    classify the code

--- video-player-shortcode.php
+++ video-player-shortcode.php
@@ -6,63 +6,69 @@
  * Author: Gabe Herbert, Kitestring Studio
  */
 
-function extract_youtube_video_id($url) {
-	$parsed_url = parse_url($url);
-
-	if ($parsed_url['host'] == 'youtu.be') {
-		return ltrim($parsed_url['path'], '/');
+class Video_Player_Shortcode {
+	public function __construct() {
+		add_shortcode('video_player', array($this, 'render_shortcode'));
 	}
 
-	if ($parsed_url['host'] == 'www.youtube.com' || $parsed_url['host'] == 'youtube.com') {
-		if ($parsed_url['path'] == '/watch') {
-			parse_str($parsed_url['query'], $query_params);
-			return $query_params['v'];
-		} elseif ($parsed_url['path'] == '/embed' || $parsed_url['path'] == '/v') {
+	private function extract_youtube_video_id($url) {
+		$parsed_url = parse_url($url);
+
+		if ($parsed_url['host'] == 'youtu.be') {
 			return ltrim($parsed_url['path'], '/');
 		}
+
+		if ($parsed_url['host'] == 'www.youtube.com' || $parsed_url['host'] == 'youtube.com') {
+			if ($parsed_url['path'] == '/watch') {
+				parse_str($parsed_url['query'], $query_params);
+				return $query_params['v'];
+			} elseif ($parsed_url['path'] == '/embed' || $parsed_url['path'] == '/v') {
+				return ltrim($parsed_url['path'], '/');
+			}
+		}
+
+		return null;
 	}
 
-	return null;
+	public function render_shortcode($atts, $content = null) {
+		$atts = shortcode_atts(
+			array(
+				'type' => 'youtube',
+				'style' => '1',
+				'dimensions' => '853x480',
+				'width' => '853',
+				'height' => '480',
+				'align' => 'center',
+				'margin_top' => '0',
+				'margin_bottom' => '20',
+				'ipad_color' => 'black'
+			),
+			$atts,
+			'video_player'
+		);
+
+		$video_url = base64_decode($content);
+		if (!filter_var($video_url, FILTER_VALIDATE_URL)) {
+			return 'Invalid YouTube URL';
+		}
+
+		$video_id = $this->extract_youtube_video_id($video_url);
+		if (empty($video_id)) {
+			return 'Invalid YouTube URL';
+		}
+
+		$width = esc_attr($atts['width']);
+		$height = esc_attr($atts['height']);
+		$align = esc_attr($atts['align']);
+		$margin_top = esc_attr($atts['margin_top']);
+		$margin_bottom = esc_attr($atts['margin_bottom']);
+
+		return "
+            <div style=\"text-align: {$align}; margin-top: {$margin_top}px; margin-bottom: {$margin_bottom}px;\">
+                <iframe width=\"{$width}\" height=\"{$height}\" src=\"https://www.youtube.com/embed/{$video_id}\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen loading=\"lazy\"></iframe>
+            </div>
+        ";
+	}
 }
 
-function video_player_shortcode($atts, $content = null) {
-	$atts = shortcode_atts(
-		array(
-			'type' => 'youtube',
-			'style' => '1',
-			'dimensions' => '853x480',
-			'width' => '853',
-			'height' => '480',
-			'align' => 'center',
-			'margin_top' => '0',
-			'margin_bottom' => '20',
-			'ipad_color' => 'black'
-		),
-		$atts,
-		'video_player'
-	);
-
-	$video_url = base64_decode($content);
-	if (!filter_var($video_url, FILTER_VALIDATE_URL)) {
-		return 'Invalid YouTube URL';
-	}
-
-	$video_id = extract_youtube_video_id($video_url);
-	if (empty($video_id)) {
-		return 'Invalid YouTube URL';
-	}
-
-	$width = esc_attr($atts['width']);
-	$height = esc_attr($atts['height']);
-	$align = esc_attr($atts['align']);
-	$margin_top = esc_attr($atts['margin_top']);
-	$margin_bottom = esc_attr($atts['margin_bottom']);
-
-	return "
-        <div style=\"text-align: {$align}; margin-top: {$margin_top}px; margin-bottom: {$margin_bottom}px;\">
-            <iframe width=\"{$width}\" height=\"{$height}\" src=\"https://www.youtube.com/embed/{$video_id}\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen loading=\"lazy\"></iframe>
-        </div>
-    ";
-}
-
-add_shortcode('video_player', 'video_player_shortcode');
+new Video_Player_Shortcode();
